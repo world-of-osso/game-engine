03908e423c9db971696b44037de5cd8bd85ef80a
Alessio Deiana <adeiana@gmail.com>
2026-03-24 12:06:31 -0500

    Wire game menu button actions: Exit, Log Out, Resume, stubs for rest
    - Exit Game sends AppExit
    - Log Out transitions to Login state
    - Return to Game transitions to InWorld
    - Escape key also resumes game
    - Options/Support/AddOns log stubs
    - Click handling walks parent chain for onclick like other screens

--- src/game_menu_screen.rs
+++ src/game_menu_screen.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
 
+use game_engine::ui::input::find_frame_at;
 use game_engine::ui::plugin::{UiState, sync_registry_to_primary_window};
-use game_engine::ui::screens::game_menu_component::{GAME_MENU_ROOT, game_menu_screen};
+use game_engine::ui::screens::game_menu_component::{
+    ACTION_ADDONS, ACTION_EXIT, ACTION_LOGOUT, ACTION_OPTIONS, ACTION_RESUME, ACTION_SUPPORT,
+    GAME_MENU_ROOT, game_menu_screen,
+};
 use game_engine::ui_resource;
 use ui_toolkit::screen::Screen;
 
@@ -29,6 +33,10 @@ impl Plugin for GameMenuScreenPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::GameMenu), build_game_menu_ui);
         app.add_systems(OnExit(GameState::GameMenu), teardown_game_menu_ui);
+        app.add_systems(
+            Update,
+            handle_menu_input.run_if(in_state(GameState::GameMenu)),
+        );
     }
 }
 
@@ -59,4 +67,67 @@ fn teardown_game_menu_ui(
     commands.remove_resource::<GameMenuScreenWrap>();
     commands.remove_resource::<GameMenuUi>();
     ui.focused_frame = None;
+}
+
+fn handle_menu_input(
+    mut ui: ResMut<UiState>,
+    mouse: Option<Res<ButtonInput<MouseButton>>>,
+    windows: Query<&Window, With<bevy::window::PrimaryWindow>>,
+    mut next_state: ResMut<NextState<GameState>>,
+    mut exit: MessageWriter<AppExit>,
+    keyboard: Option<Res<ButtonInput<KeyCode>>>,
+) {
+    if let Some(ref kb) = keyboard {
+        if kb.just_pressed(KeyCode::Escape) {
+            next_state.set(GameState::InWorld);
+            return;
+        }
+    }
+    let Some(mouse) = mouse else { return };
+    if !mouse.just_pressed(MouseButton::Left) {
+        return;
+    }
+    let Ok(window) = windows.single() else { return };
+    let Some(pos) = window.cursor_position() else { return };
+    let Some(frame_id) = find_frame_at(&ui.registry, pos.x, pos.y) else { return };
+    let action = walk_up_for_onclick(&ui.registry, frame_id);
+    if let Some(action) = action {
+        dispatch_action(&action, &mut next_state, &mut exit);
+    }
+    ui.registry.click_frame(frame_id);
+}
+
+fn walk_up_for_onclick(
+    reg: &game_engine::ui::registry::FrameRegistry,
+    mut id: u64,
+) -> Option<String> {
+    loop {
+        if let Some(frame) = reg.get(id) {
+            if let Some(ref onclick) = frame.onclick {
+                return Some(onclick.clone());
+            }
+            match frame.parent_id {
+                Some(pid) => id = pid,
+                None => return None,
+            }
+        } else {
+            return None;
+        }
+    }
+}
+
+fn dispatch_action(
+    action: &str,
+    next_state: &mut NextState<GameState>,
+    exit: &mut MessageWriter<AppExit>,
+) {
+    match action {
+        ACTION_EXIT => { exit.write(AppExit::Success); }
+        ACTION_LOGOUT => { next_state.set(GameState::Login); }
+        ACTION_RESUME => { next_state.set(GameState::InWorld); }
+        ACTION_OPTIONS => info!("Options: not implemented yet"),
+        ACTION_SUPPORT => info!("Support: not implemented yet"),
+        ACTION_ADDONS => info!("AddOns: not implemented yet"),
+        _ => warn!("Unknown menu action: {action}"),
+    }
 }
