41df65f037929ee6c4a8119ecd48c6300e1a5263
Alessio Deiana <adeiana@gmail.com>
2026-04-04 00:15:09 -0500

    Split M2 key bone name helpers

--- src/asset/m2_format/m2_bone_names.rs
+++ src/asset/m2_format/m2_bone_names.rs
@@ -1,39 +1,58 @@
 /// Map WoW M2 key_bone_id to a human-readable name.
 pub fn key_bone_name(id: i32) -> &'static str {
+    upper_body_key_bone_name(id)
+        .or_else(|| hand_key_bone_name(id))
+        .or_else(|| misc_key_bone_name(id))
+        .unwrap_or("bone")
+}
+
+fn upper_body_key_bone_name(id: i32) -> Option<&'static str> {
     match id {
-        0 => "ArmL",
-        1 => "ArmR",
-        2 => "ShoulderL",
-        3 => "ShoulderR",
-        4 => "SpineLow",
-        5 => "Waist",
-        6 => "Head",
-        7 => "Jaw",
-        8 => "IndexFingerR",
-        9 => "MiddleFingerR",
-        10 => "PinkyFingerR",
-        11 => "RingFingerR",
-        12 => "ThumbR",
-        13 => "IndexFingerL",
-        14 => "MiddleFingerL",
-        15 => "PinkyFingerL",
-        16 => "RingFingerL",
-        17 => "ThumbL",
-        18 => "BTH",
-        19 => "CSR",
-        20 => "CSL",
-        21 => "Breath",
-        22 => "Root",
-        23 => "Knee",
-        24 => "FootL",
-        25 => "FootR",
-        26 => "ElbowL",
-        27 => "ElbowR",
-        28 => "KneeR",
-        29 => "KneeL",
-        30 => "WHL",
-        31 => "WHR",
-        _ => "bone",
+        0 => Some("ArmL"),
+        1 => Some("ArmR"),
+        2 => Some("ShoulderL"),
+        3 => Some("ShoulderR"),
+        4 => Some("SpineLow"),
+        5 => Some("Waist"),
+        6 => Some("Head"),
+        7 => Some("Jaw"),
+        _ => None,
+    }
+}
+
+fn hand_key_bone_name(id: i32) -> Option<&'static str> {
+    match id {
+        8 => Some("IndexFingerR"),
+        9 => Some("MiddleFingerR"),
+        10 => Some("PinkyFingerR"),
+        11 => Some("RingFingerR"),
+        12 => Some("ThumbR"),
+        13 => Some("IndexFingerL"),
+        14 => Some("MiddleFingerL"),
+        15 => Some("PinkyFingerL"),
+        16 => Some("RingFingerL"),
+        17 => Some("ThumbL"),
+        _ => None,
+    }
+}
+
+fn misc_key_bone_name(id: i32) -> Option<&'static str> {
+    match id {
+        18 => Some("BTH"),
+        19 => Some("CSR"),
+        20 => Some("CSL"),
+        21 => Some("Breath"),
+        22 => Some("Root"),
+        23 => Some("Knee"),
+        24 => Some("FootL"),
+        25 => Some("FootR"),
+        26 => Some("ElbowL"),
+        27 => Some("ElbowR"),
+        28 => Some("KneeR"),
+        29 => Some("KneeL"),
+        30 => Some("WHL"),
+        31 => Some("WHR"),
+        _ => None,
     }
 }
 
