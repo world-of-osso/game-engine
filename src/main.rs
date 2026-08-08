045381165f1497bfa689ebb862c53bed8608c54d
Alessio Deiana <adeiana@gmail.com>
2026-08-08 13:35:09 -0500

    Revert "Add M2 UV update diagnostic selector"
    This reverts commit b9c81e1aa6da862c2a2ef5360435464887062e4d.

--- src/main.rs
+++ src/main.rs
@@ -398,14 +398,7 @@ fn insert_startup_resources(
         app.insert_resource(scenes::char_select::PreselectedCharName(name));
     }
     insert_screen_resources(app, args);
-    insert_m2_effect_uv_update_resource(app, args);
     insert_data_resources(app);
-}
-
-fn insert_m2_effect_uv_update_resource(app: &mut App, args: &[String]) {
-    if has_flag(args, "--disable-m2-effect-uv-updates") {
-        app.insert_resource(m2_effect_material::M2EffectUvUpdatesEnabled(false));
-    }
 }
 
 fn insert_screen_resources(app: &mut App, args: &[String]) {
