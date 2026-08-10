b264c78c541c103caa4d9721c34dfbbcc3ef0467
Alessio Deiana <adeiana@gmail.com>
2026-08-10 18:57:32 -0500

    Initialize Bevy 0.19 FPS shader buffers in tests

--- tests/unit/main_tests.rs
+++ tests/unit/main_tests.rs
@@ -102,6 +102,7 @@ fn pre_ui_processing_gate_keeps_fps_overlay_active() {
     app.add_plugins(bevy::asset::AssetPlugin::default());
     app.add_plugins(bevy::text::TextPlugin::default());
     app.init_asset::<bevy::shader::Shader>();
+    app.init_asset::<bevy::render::storage::ShaderBuffer>();
     app.add_plugins(FpsOverlayPlugin {
         config: FpsOverlayConfig {
             refresh_interval: Duration::from_secs(1),
@@ -131,6 +132,7 @@ fn character_stage_preserves_fps_overlay_graph() {
     app.add_plugins(bevy::asset::AssetPlugin::default());
     app.add_plugins(bevy::text::TextPlugin::default());
     app.init_asset::<bevy::shader::Shader>();
+    app.init_asset::<bevy::render::storage::ShaderBuffer>();
     app.add_plugins(FpsOverlayPlugin::default());
     app.insert_resource(InWorldSceneStage::Character);
 
